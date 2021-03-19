1b7e63ae9ea4b4acb9371c3ad83898f055980b88
Genn <intval@i.ua>
2021-03-19 23:46:07 +0200

    start

---
+++ src/imgs/Images.js
@@ -0,0 +1,8 @@
+import './Images.css'
+
+function Images() {
+    return(
+        <img alt="img" className="pics" src="../pics/glasses.jpg"/>
+    );
+}
+export default Images;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Headmenu from './headmenu/Headmenu';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Headmenu />
   </React.StrictMode>,
   document.getElementById('root')
 );
